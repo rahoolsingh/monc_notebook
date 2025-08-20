119597f3f69141f82a39b0af191d4f4e989c8833
Veer Rajpoot <rahulksingh3907@gmail.com>
2025-08-20 12:06:02 +0530

    feat: enhance file upload handling to support PDF vectorization and indexing

--- backend/src/controller.js
+++ backend/src/controller.js
@@ -1,15 +1,33 @@
 import "dotenv/config";
-import OpenAI from "openai";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "@langchain/openai";
 import { QdrantVectorStore } from "@langchain/qdrant";
-
-
 
 export const uploadFiles = async (req, res) => {
     const filename = req.file.filename;
+    const filePath = req.file.path;
+    const fileType = req.file.mimetype;
+
+    if (fileType === "application/pdf") {
+        const loader = new PDFLoader(filePath);
+
+        const docs = await loader.load();
+
+        const embeddings = new OpenAIEmbeddings({
+            model: "text-embedding-3-large",
+        });
+
+        await QdrantVectorStore.fromDocuments(docs, embeddings, {
+            url: "http://localhost:6333",
+            collectionName: "chaicode-collection",
+        });
+    } else {
+        console.log(`Unsupported file type: ${fileType}`);
+    }
 
     res.status(200).json({
         success: true,
-        message: "File uploaded successfully!",
+        message: "Vectorization and indexing done successfully!",
         data: {
             filename,
         },
